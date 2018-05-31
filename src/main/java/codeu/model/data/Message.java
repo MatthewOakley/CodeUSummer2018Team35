04abd035d2354bacf8e5891bfdfbd5a6f5e6fd67
ilianac <idc2117@columbia.edu>
2018-05-31 11:48:10 -0500

    added test for emojis

--- src/main/java/codeu/model/data/Message.java
+++ src/main/java/codeu/model/data/Message.java
@@ -73,7 +73,6 @@ public class Message {
     return renderer.render(document);
   }
 
-
   /** Returns the creation time of this Message. */
   public Instant getCreationTime() {
     return creation;
